12dd74f93afa8cae7a2dfa7e1d5c7f9be0de4dc9
N0ctane._.Dev <n0ctaneDev@gmail.com>
2026-05-25 10:29:55 +0530

    Update by N0ctane._.Dev at 2026-05-25/10-29-55

--- n0ctos-st/config.h
+++ n0ctos-st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "ProFontWindows Nerd Font Propo:pixelsize=14:antialias=true:autohint=true";
+static char *font = "ProFontWindows Nerd Font Propo:size=12:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
